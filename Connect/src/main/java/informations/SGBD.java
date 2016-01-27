c04effb804a9ebf2bba117f7e90845d55a2342ae
SIMON LAMOURE <Simon@macbook-pro-de-simon.home>
2016-01-27 17:06:16 +0100

    update SGBD / Serveur
    ajout classe informations.SGBD
    ajout méthode sgbd.récupérerMail
    
    update de mon workspace sur GitHub plutôt qu’en local (je veux
    expérimenter vos problèmes)

---
+++ Connect/src/main/java/informations/SGBD.java
@@ -0,0 +1,16 @@
+package informations;
+
+public class SGBD {
+	
+	private boolean testRecuperation;
+	
+	// Exemple de méthode SGBD
+	public boolean recupererMail(String adresseMail){
+		
+		//testRecuperation = Requete SQL getMail
+		if (testRecuperation){
+			return true;
+		} else
+			return false;
+	}
+}
