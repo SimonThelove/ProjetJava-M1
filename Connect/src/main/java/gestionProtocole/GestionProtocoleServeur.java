4ba8012fceef8075a3a3026be7a7b9a8b95856fa
josselynL <loulier.j@gmail.com>
2016-01-27 15:32:52 +0100

    Modif GPserveur

--- Connect/src/main/java/gestionProtocole/GestionProtocoleServeur.java
+++ Connect/src/main/java/gestionProtocole/GestionProtocoleServeur.java
@@ -15,55 +15,76 @@ public class GestionProtocoleServeur {
     }
 
     public String requete(String entreeSocket){
-        String[] req = entreeSocket.split(" ");
+        String[] req = entreeSocket.split("|");
         switch(req[0]){
         case "CREA":
             try {
                 serveur.creerCompte(req[1], Double.parseDouble(req[2]));
-                return "MSG|Votre compte a bien été créé, vous pouvez maintenant vous connecter.";
+                return "MSG|Votre compte a bien ete cree, vous pouvez maintenant vous connecter.";
             } catch (NumberFormatException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                return "MSG| Erreur format";
+                return "MSG|Erreur lors de la creation de votre compte.";
             }
         case "CONX":
-                return "POS " + serveur.getSolde(req[1]) + serveur.getDerniereOperation(req[1]);
+        	try {
+                serveur.seConnecter(req[1], Double.parseDouble(req[2]));
+                return "MSG|Vous etes bien connecte, que souhaitez-vous faire ?";
+            } catch (NumberFormatException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return "MSG|Erreur dans la combinaison mail / mot de passe, veuillez reessayer.";
+            }
              
         case "MODI":
             try {
-                serveur.ajouter(req[1], Double.parseDouble(req[2]));
-                return "OK";
+                serveur.modifierInformations(req[1], Double.parseDouble(req[2]));
+                return "MSG|Vos informations ont bien ete mises � jour.";
             } catch (NumberFormatException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                return "MSG| Erreur format";
+                return "MSG|Erreur lors de la mise � jour de vos informations.";
             }
         case "CONS":
             try {
-                serveur.retirer(req[1], Double.parseDouble(req[2]));
-                return "OK";
+                serveur.rechercherChamps(req[1], Double.parseDouble(req[2]));
+                return "--------------------------------";
             } catch (NumberFormatException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                return "MSG| Erreur format";
+                return "MSG|Erreur lors de la recherche.";
             }
             case "RECH":
-            try {
-                serveur.retirer(req[1], Double.parseDouble(req[2]));
-                return "OK";
-            } catch (NumberFormatException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                return "MSG| Erreur format";
-            }
+            	if(req[1]=="MOTSCLES")
+            	{
+            		 try {
+                         serveur.rechercherMotsCles(req[1], Double.parseDouble(req[2]));
+                         return "-------------------------------";
+                     } catch (NumberFormatException e) {
+                         // TODO Auto-generated catch block
+                         e.printStackTrace();
+                         return "MSG|Erreur lors de votre recherche.";
+                     }
+            	}
+            	else if(req[1]=="NOM")
+            	{
+            		 try {
+                         serveur.rechercherChamps(req[1], Double.parseDouble(req[2]));
+                         return "-------------------------------";
+                     } catch (NumberFormatException e) {
+                         // TODO Auto-generated catch block
+                         e.printStackTrace();
+                         return "MSG|Erreur lors de votre recherche.";
+                     }
+            	}
             case "DECO":
             try {
-                serveur.retirer(req[1], Double.parseDouble(req[2]));
-                return "OK";
+                serveur.seDeconnecter(req[1], Double.parseDouble(req[2]));
+                return "MSG|Vous etes bien deconnecte.";
             } catch (NumberFormatException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                return "MSG| Erreur format";
+                return "MSG|Erreur lors de la deconnexion";
             }
         default :
             return "MSG|Erreur dans votre choix";
