ea50ea2909d45dc6965a3a3f056a2d192eb93fc8
SIMON LAMOURE <Simon@macbook-pro-de-simon.home>
2016-01-31 18:46:10 +0100

    creation test Yohann

---
+++ Connect/src/test/java/testClient/TestYohann.java
@@ -0,0 +1,12 @@
+package testClient;
+import client.Client;
+import gestionProtocole.GestionProtocoleClient;
+
+public class TestYohann {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
