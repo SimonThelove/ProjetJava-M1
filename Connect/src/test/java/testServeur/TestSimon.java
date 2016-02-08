63d5e8ee513c1169236207814364addc5531a30d
SIMON LAMOURE <Simon@macbook-pro-de-simon.home>
2016-02-08 21:42:59 +0100

    test serveur

--- Connect/src/test/java/testServeur/TestSimon.java
+++ Connect/src/test/java/testServeur/TestSimon.java
@@ -1,10 +1,17 @@
 package testServeur;
 
+import socketsTCP.SocketServeur;
+
 public class TestSimon {
+	
+	private static SocketServeur soc;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		SocketServeur soc = new SocketServeur();
+		
+		soc.socket();
 	}
 
 }
